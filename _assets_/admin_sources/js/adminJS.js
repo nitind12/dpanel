cc594f954baa6186083603b16bd45d01bb31b605
Dr. Naveen Tewari <navtewari@gmail.com>
2018-02-07 12:36:26 +0530

    up gallery
    up gallery

--- _assets_/admin_sources/js/adminJS.js
+++ _assets_/admin_sources/js/adminJS.js
@@ -11,9 +11,9 @@ function  loadgallery(dataCmb) {
             var id = $(this).attr('id');
             //$("#" + id + "g").hide();
             $.ajax({
-                url: site_url_ + "/gallery/deleteimg",
-                data: 'id=' + id,
-                type: 'POST'
+                url: site_url_ + "/gallery/deleteimg/"+ id,
+                //data: 'id=' + id,
+                type: 'GET'
             }).done(function (data) {
                 loadgallery(dataCmb);
             });
@@ -25,9 +25,9 @@ function  loadgallery(dataCmb) {
             e.preventDefault();
             var id = $(this).attr('id');
             $.ajax({
-                url: site_url_ + "/gallery/activeImg",
-                data: 'id=' + id,
-                type: 'POST'
+                url: site_url_ + "/gallery/activeImg/" + id,
+               // data: 'id=' + id,
+                type: 'GET'
             }).done(function (data) {
                 loadgallery(dataCmb);
             });
@@ -39,9 +39,9 @@ function  loadgallery(dataCmb) {
             e.preventDefault();
             var id = $(this).attr('id');
             $.ajax({
-                url: site_url_ + "/gallery/InactiveImg",
-                data: 'id=' + id,
-                type: 'POST'
+                url: site_url_ + "/gallery/InactiveImg/"+id,
+               // data: 'id=' + id,
+                type: 'GET'
             }).done(function (data) {
                 loadgallery(dataCmb);
             });
@@ -67,6 +67,15 @@ function change_Cat(id_, val_, val2_) {
     document.getElementById('txtCategory_edit').value = val_;
     document.getElementById('txtDesc_edit').value = val2_;
     document.getElementById('txtID_edit').value = id_;
+    document.getElementById('editCat').style.display = 'block';
+    document.getElementById('newCat').style.display = 'none';
+}
+
+function change_Report(id_, val_, path_) {
+    document.getElementById('txtReportYear_edit').value = val_;
+    document.getElementById('txtID_edit').value = id_;
+    $("#Plist").html(path_);
+     $("a[href='mylist']").attr('href', base_url_ + "_assets_/annualReport/" + path_);
     document.getElementById('editCat').style.display = 'block';
     document.getElementById('newCat').style.display = 'none';
 }
---
+++ application/controllers/Gallery.php
@@ -0,0 +1,111 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Gallery extends CI_Controller {
+
+    function __construct(){
+        parent::__construct();
+        $this->load->model('My_model_gallery', 'mmg');
+		$this->load->model('My_model', 'mm');
+        if (! $this->session->userdata('ussr_')) {
+            redirect(__BACKTOSITE__);
+        }
+    }
+    function index() {
+        $data['user___'] = $this->session->userdata('ussr_');
+        $data['existing'] = $this->mmg->get_all_categories();
+		$data['menu'] = $this->mm->get_menu($this->session->userdata('stss_'));
+		$data['innerst'] ='0';
+        $data['folder_'] = 'gallery';
+        $data['page__'] = 'feedgallery';
+        $data['page_head'] = 'Upload &amp; Manage Gallery';
+        $data['view1'] = 'Updategallery';
+		$data['wallpaper_'] = '';
+
+         $this->load->view('templates/header', $data);
+        $this->load->view('inner', $data);
+        $this->load->view('templates/footer');
+    }
+
+    function feedCategory() {
+        $res_ = $this->mmg->feedCategory_();
+        $this->session->set_flashdata('_msg_', $res_['msg_']);
+        redirect('gallery');
+    }
+
+    function editCategory() {
+        $res_ = $this->mmg->editCategory_();
+        $this->session->set_flashdata('_msg_', $res_['msg_']);
+        redirect('gallery');
+    }
+
+    function active_inactive($id_, $status) {
+        $res_ = $this->mmg->active_inactive_($id_, $status);
+        redirect('gallery');
+    }
+
+    function deleteCat($id_) {
+        $res_ = $this->mmg->deletecateg($id_);
+        $this->session->set_flashdata('_msg_', $res_['msg_']);
+        redirect('gallery');
+    }
+
+    function getImages() {
+        $data['existing'] = $this->mmg->getImagesByCat(trim($this->input->post('txtCategory')));
+        echo json_encode($data['existing']);
+    }
+
+    function uploadGallery() {
+        $data['existing'] = $this->mmg->uploadGallery_(trim($this->input->post('txtCategory')));
+        echo json_encode($data['existing']);
+        //redirect('admin_/gallery');
+    }
+
+    //---------------------------------------------------------------------------------------------
+
+    function do_upload(){
+        $this->mmg->do_upload();
+        redirect('gallery');
+    }
+
+    function fillGallery($id_) {
+        $data_to_ajax = $this->mmg->fillGallery($id_);
+        echo $data_to_ajax;
+    }
+
+    function deleteimg($id) {
+        $uploadpath = FCPATH . '_assets_/gallery/';
+        $row = $this->mmg->deleteimg($id);
+
+        $src = $uploadpath . $row['photo__'];
+        @unlink($src);
+
+        if($row['res_'] == TRUE){
+            echo'<h4 style="color:green">This image deleted successfully</h4>';
+        } else {
+            echo'<h4 style="color:green">Some server error !! Please try again.</h4>';
+        }
+    }
+
+    function activeImg($id) {
+        $bool_ = $this->mmg->activeImg($id);
+
+        if($bool_ == TRUE){
+            echo'<h4 style="color:green">This image deleted successfully</h4>';    
+        } else {
+            echo'<h4 style="color:green">Some server error !! Please try again.</h4>';    
+        }
+    }
+
+    function InactiveImg($id) {
+        $bool_ = $this->mmg->InactiveImg($id);
+
+        if($bool_ == TRUE){
+            echo'<h4 style="color:green">This image deleted successfully</h4>';    
+        } else {
+            echo'<h4 style="color:green">Some server error !! Please try again.</h4>';    
+        }
+    }
+
+}
